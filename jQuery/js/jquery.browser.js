27085cbde360bf3a28472e05c3c4b1596fdda24b
zhoubing <254784109@qq.com>
2016-04-28 18:17:26 +0800

    add

--- jQuery/js/jquery.browser.js
+++ jQuery/js/jquery.browser.js
@@ -1,37 +1,35 @@
 ;(function ($) {
 
-    var userAgent = navigator.userAgent;
+	var userAgent = navigator.userAgent;
 
-    $.browser = {};
+	$.browser = {};
 
-    function uaMatch(ua) {
-        ua = ua.toLowerCase();
+	function uaMatch(ua) {
+		ua = ua.toLowerCase();
 
-        var rwebkit = /(webkit)[ \/]([\w.]+)/,
-            ropera = /(opera)(?:.*version)?[ \/]([\w.]+)/,
-            rmsie = /(msie) ([\w.]+)/,
-            rmozilla = /(mozilla)(?:.*? rv:([\w.]+))?/;
+		var rwebkit  = /(webkit)[ \/]([\w.]+)/,
+		    ropera   = /(opera)(?:.*version)?[ \/]([\w.]+)/,
+		    rmsie    = /(msie) ([\w.]+)/,
+		    rmozilla = /(mozilla)(?:.*? rv:([\w.]+))?/;
 
-        var match = rwebkit.exec(ua) ||
-            ropera.exec(ua) ||
-            rmsie.exec(ua) ||
-            ua.indexOf("compatible") < 0 && rmozilla.exec(ua) ||
-            [];
+		var match = rwebkit.exec(ua) ||
+			ropera.exec(ua) ||
+			rmsie.exec(ua) ||
+			ua.indexOf("compatible") < 0 && rmozilla.exec(ua) ||
+			[];
 
-        return {browser: match[1] || "", version: match[2] || "0"};
-    }
+		return {browser: match[1] || "", version: match[2] || "0"};
+	}
 
+	var browserMatch = uaMatch(userAgent);
 
-    var browserMatch = uaMatch(userAgent);
+	if (browserMatch.browser) {
+		$.browser[browserMatch.browser] = true;
+		$.browser.version = browserMatch.version;
+	}
 
-    if (browserMatch.browser) {
-        $.browser[browserMatch.browser] = true;
-        $.browser.version = browserMatch.version;
-    }
-
-    if ($.browser.webkit) {
-        $.browser.safari = true;
-    }
-
+	if ($.browser.webkit) {
+		$.browser.safari = true;
+	}
 
 })(jQuery);
